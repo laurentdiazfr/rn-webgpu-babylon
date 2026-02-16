9f50d7064eeda97185ecc67757a073bc0ae66de9
Laurent DIAZ <laurentdiazfr@gmail.com>
2026-02-16 18:59:32 +0100

    another typo

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import BabylonCube from './components/BabylonCube';
+import BabylonCube from './demos/BabylonCube';
 export default function App() {
   return (
     <View style={styles.container}>
